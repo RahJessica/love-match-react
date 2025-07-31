9572dca900a8e910fb82a782ce356f6848b6fd9b
RahJess <jessicakantotiana@gmail.com>
2025-07-31 18:10:32 +0200

    feat: add logic part to test compatibility and imrpove design's form

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,7 @@ import SplitText from './components/SplitText';
 
 export default function App() {
   return (
-    <main className="w-full h-screen flex items-center justify-center">
-       <SplitText
-        text="Hello les K1, how are you?"
-        className="text-5xl font-semibold text-center"
-        delay={100}
-        duration={0.6}
-        ease="power3.out"
-        splitType="chars"
-        from={{ opacity: 0, y: 40 }}
-        to={{ opacity: 1, y: 0 }}
-        threshold={0.1}
-        rootMargin="-100px"
-        textAlign="center"
-      />
+    <main className="w-full h-screen flex items-center justify-center bg-gradient-to-r from-[#ff758f] via-[#ffb3c1] to-[#ff758f]">
       <FormUser/>
     </main>
   );
--- src/components/formUser.tsx
+++ src/components/formUser.tsx
@@ -1,27 +1,69 @@
-import React, { useRef, useEffect } from 'react';
-import { gsap } from 'gsap';
-import { div } from 'motion/react-client';
+import React, { useRef, useEffect, useState, Fragment } from 'react';
 import { FaHeart } from 'react-icons/fa';
 
 function FormUser() {
+
+    const [firstname, setFirstName] = useState('')
+    const [secondName, setSecondName] = useState('')
+    const [stat, setStat] = useState<number | null>(null)
+    const [isMatched, setIsMatched] = useState(false)
+
+    const handleChangeName = (e) => {
+        setFirstName(e.target.value)
+    }
+
+    const handleChangeSecondName = (e) => {
+        setSecondName(e.target.value)
+    }
+
+    const MatchStat = () => {
+        const randomStat = Math.floor(Math.random() * 101)
+        setStat(randomStat)
+        setIsMatched(true)
+    }
+
+    const Matched = () => {
+        setIsMatched(true)
+        setStat(null)
+    }
+
+    useEffect(() => {
+        setIsMatched(false);
+        setStat(null);
+    }, [firstname, secondName]);
+
     return (
-        <div className='flex flex-col justify-center items-center gap-5 border-2 border-gray-600 mx-30 my-20 px-10 py-5 w-[30vw] h-[70vh] rounded-lg shadow-gray-800'>
-            <FaHeart color="red" 
-                className='w-[10vw] h-[20vh]'
-            />
-            <h1 className='text-center text-2xl'>Love match</h1>
-            <p className='text-center'>Découvrez votre partenaire idéal</p>
-            <input type="text" 
-            placeholder='Enter your name'
-            className='w-[25vw] border border-gray-900 px-2 py-1 rounded-lg'
-            />
-            <p className='text-center'>and</p>
-            <input type="text" 
-            placeholder="Enter your crush's name"
-            className='w-[25vw] border border-gray-900 px-2 py-1 rounded-lg'
-            />
-            <button className='bg-red-400 cursor-pointer w-20 py-2 rounded-2xl transform transition duration-200 ease-in-out hover:scale-105'>Match</button>
-        </div>
+        <>
+
+            <div className='flex flex-col justify-center items-center gap-4 bg-white mx-30 my-20 px-10 py-5 w-[30vw] h-[75vh] rounded-3xl shadow-2xl'>
+                <FaHeart
+                    className='w-[6vw] h-[7vh] text-[#ff4d6d]'
+                />
+                <h1 className='text-center text-2xl'>Love match</h1>
+                <p className='text-center'>Find your soulmate in one click !</p>
+                <input type="text"
+                    placeholder='Enter your name'
+                    name='firstName'
+                    className='w-[25vw] border border-gray-900 px-2 py-1 rounded-sm'
+                    onChange={handleChangeName}
+                />
+                <p className='text-center'>and</p>
+                <input type="text"
+                    placeholder="Enter your crush's name"
+                    name='secondName'
+                    onChange={handleChangeSecondName}
+                    className='w-[25vw] border border-gray-900 px-2 py-1 rounded-sm'
+                />
+                <button
+                    onClick={MatchStat}
+                    disabled={!firstname || !secondName || isMatched}
+                    className='bg-[#f9dbbd] cursor-pointer w-80 py-2 mt-2 rounded-xl transform transition duration-200 ease-in-out hover:scale-95'>
+                    Test compatibility 👀
+                </button>
+                {stat != null && <p>Vous êtes compatibles à : {stat}%</p>}
+
+            </div>
+        </>
     )
 }
 
